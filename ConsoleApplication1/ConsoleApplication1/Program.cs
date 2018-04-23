3d0c1981425aec870d2aa1e6e28384a04366d064
Алина <ovchinnikowa2013@yandex.ru>
2018-04-23 19:30:17 +0700

    Добавлен проект 1, переименованы решение и проект.

--- ConsoleApplication1/ConsoleApplication1/Program.cs
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -4,12 +4,58 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ConsoleApplication1
-{
-    class Program
+namespace part1
     {
-        static void Main(string[] args)
+        class Program
         {
+            static void initializationMatrix(int[,] matrix)
+            {
+                Random rnd = new Random();
+                for (int i = 0; i < matrix.GetLength(0); i++)
+                    for (int j = 0; j < matrix.GetLength(1); j++)
+                        matrix[i, j] = rnd.Next(10, 99);
+            }
+
+            static void printMatrix(int[,] matrix)
+            {
+                for (int i = 0; i < matrix.GetLength(0); i++)
+                {
+                    for (int j = 0; j < matrix.GetLength(1); j++)
+                        Console.Write($"{matrix[i, j]} ");
+                    Console.WriteLine();
+                }
+                Console.WriteLine();
+            }
+
+            static void swapRow(int[,] matrix)
+            {
+                for (int i = 0, l = matrix.GetLength(0) - 1; i < l; i++, l--)
+                {
+                    for (int j = 0; j < matrix.GetLength(1); j++)
+                    {
+                        int temp = matrix[i, j];
+                        matrix[i, j] = matrix[l, j];
+                        matrix[l, j] = temp;
+                    }
+                }
+            }
+
+            static void Main(string[] args)
+            {
+                Console.Write("Size n=");
+                int n = Convert.ToInt32(Console.ReadLine());
+
+                Console.Write("Size m=");
+                int m = Convert.ToInt32(Console.ReadLine());
+
+                int[,] matrix = new int[n, m];
+
+                initializationMatrix(matrix);
+                printMatrix(matrix);
+
+                swapRow(matrix);
+                printMatrix(matrix);
+            }
         }
     }
-}
+
